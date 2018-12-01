5d0803f2a02830ff52765ddcd92b8bbe517d29f1
Şahin Olut <olut.sahin@gmail.com>
2018-12-01 22:43:03 +0300

    Axios test with cloud

--- MercedesRequest/index.js
+++ MercedesRequest/index.js
@@ -1,16 +1,24 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     const MercedesHelper = require('./helpers/mercedes')
+    const axios = require('axios');
     if (req.query.name || (req.body && req.body.name)) {
+        try {
+
+            const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            console.log(res.data)
+
+            context.res = {
+                status: 200,
+                body: JSON.stringify(res.data)
+            };
+        } catch {
+            throw Error("Request failure");
+        }
+    } else {
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body:  JSON.stringify(MercedesHelper.myFoo('w'))
+            status: 200,
+            body: JSON.stringify(MercedesHelper.myFoo('w'))
 
         };
     }
