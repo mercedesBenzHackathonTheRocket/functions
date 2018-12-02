3e4e62b0bd37a0ba3a4f037d881afd12e34b8e1d
Şahin Olut <olut.sahin@gmail.com>
2018-12-02 03:42:38 +0300

    Image request helper is refactored

--- ImageRequest/helpers/graphql.js
+++ ImageRequest/helpers/graphql.js
@@ -1,18 +1,52 @@
 const URLS = {
     graphql: 'https://gpgsql.herokuapp.com/v1alpha1/graphql'
 }
-async function getImages(imageType = 'truck_images', imageId) {
+async function getImages(imageType = 'truck_images', id) {
+    const axios = require('axios');
+    try {
+        if (imageType == 'truck_images') {
+            return await getTruckImages(id)
+        } else if (imageType == 'driver_images') {
+            return await getDriverImages(id)
+        }
+    } catch {
+        return 'errrrr'
+    }
+
+
+}
+async function getDriverImages(driverId) {
     const axios = require('axios');
     try {
         const res = await axios.post(URLS.graphql, {
-            "query": `query {${imageType}(where: {id: {_eq :${imageId}}}) { id url } }`
+            "query": `query {
+                drivers(
+                     where: { id: { _eq: ${driverId}}} 
+                  ) {
+                  image_url 
+                }
+              }`
+
+        })
+        return res.data
+
+    } catch {
+        return "err"
+
+    }
+}
+async function getTruckImages(imageId) {
+    const axios = require('axios');
+
+    try {
+        const res = await axios.post(URLS.graphql, {
+            "query": `query {truck_images(where: {id: {_eq :${imageId}}}) { id url } }`
 
         })
         return res.data
     } catch {
         return "err"
     }
-
 }
 module.exports = {
     getImages,
